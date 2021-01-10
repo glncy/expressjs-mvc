aa9d733a920f08ca6592b0b9a84a82cec5c2f86a
Glency Tirao <glnse@glnse.local>
2021-01-10 21:42:11 +0800

    Fix app.js broken issue

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var cors = require('cors')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 // APIs
 var indexRouterApi = require('./routes/api/index');
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // API
 app.use('/api', indexRouterApi);
